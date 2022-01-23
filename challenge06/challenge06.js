07b862691e8a749cea8c02bf542a154d6c1e712e
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-01-23 23:46:52 +0200

    fix code

--- challenge06/challenge06.js
+++ challenge06/challenge06.js
@@ -1,13 +1,5 @@
 "use strict";
 
-// 1) ---------------------
-//
-//  using the (findMax) function; return the maximum value in the provided array
-//  without using build-in functions.
-//  EX:
-//  [12, 32, 22, 45, 78, 12, 50] ==> 78
-//
-
 const findMax = (arr) => {
   let max = arr[0];
   for (let i = 0; i < arr.length; i++) {
@@ -17,15 +9,6 @@ const findMax = (arr) => {
   }
   return max;
 };
-
-// 2) ---------------------
-//
-//  using the (sumNums) function; return the Sum value of the numeric values
-//  inside the array (there could be strings inside), without using build-in functions
-//  EX:
-//  [20, '234', 'car', 41, 20, 'chair'] ==> 81
-//
-// ------------------------
 
 const sumNums = (arr) => {
   let Sum = 0;
@@ -37,14 +20,6 @@ const sumNums = (arr) => {
   return Sum;
 };
 
-// 3) ---------------------
-//
-//  using the (reverseArray) function; return provided array reversed
-//  without using build-in functions
-//  EX:
-//  ['C#', 'JS', 'Ruby','Python' ] ==> ['Python','Ruby','JS','C#']
-//
-// ------------------------
 const reverseArray = (arr) => {
   let newArray = [];
   for (i = arr.length - 1; i >= 0; i--) {
