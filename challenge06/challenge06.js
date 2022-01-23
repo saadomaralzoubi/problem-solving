38dcc2e294bc82e2171bcc0c2329b83bb56eb409
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-01-23 23:42:06 +0200

    fix code

--- challenge06/challenge06.js
+++ challenge06/challenge06.js
@@ -10,15 +10,13 @@
 // ------------------------
 
 const findMax = (arr) => {
-  const findMax = (arr) => {
-    let max = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > max) {
-        max = arr[i];
-      }
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max;
-  };
+  }
+  return max;
 };
 
 // 2) ---------------------
